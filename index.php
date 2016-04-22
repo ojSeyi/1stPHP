1d3c7ad2bb5d7c4cc0a8b1daa5bd323e2b94a849
ojSeyi <1509941@rgu.ac.uk>
2016-04-22 12:48:55 +0100

    Home page

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
         print "You can buy specs, mugs and sausage rolls";
     }
     elseif ($myage >= 18){
-        print "You can be specs and mugs";
+        print "You can buy specs and mugs";
     }
     elseif($myage >= 16){
         print "You can buy specs";
