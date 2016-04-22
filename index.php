31ee39829aad8b682715f1af230232398afa4c2b
ojSeyi <1509941@rgu.ac.uk>
2016-04-22 12:27:38 +0100

    Home page

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
     <?php
     echo "Hello World";
     echo "Hello" . " " . "World"."!";
-    echo 5*7
+    echo 5*7;
+    echo "<br>";
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . "and i am " . $myage;
     ?>
 </p>
 </body>
